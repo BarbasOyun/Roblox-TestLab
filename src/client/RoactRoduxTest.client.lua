2bca15761ac80f5da625762e9a80e58254fa4757
BarbasOyun <barbasoyun@gmail.com>
2026-05-25 20:38:32 +0200

    RoactRodux

---
+++ src/client/RoactRoduxTest.client.lua
@@ -0,0 +1,140 @@
+--RoactRodux Example : https://roblox.github.io/roact-rodux/guide/usage/
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Players = game:GetService("Players")
+local PlayerGui = Players.LocalPlayer.PlayerGui
+
+local Rodux = require(ReplicatedStorage.Shared.Rodux)
+local RoactRodux = require(ReplicatedStorage.Shared.RoactRodux)
+local Roact = require(ReplicatedStorage.Shared.Roact)
+
+-- RODUX ACTION
+
+local function IncrementAction()
+	return {
+		type = "IncrementAction",
+	}
+end
+
+local function TimeAction()
+	return {
+		type = "TimeAction",
+	}
+end
+
+-- RODUX REDUCER
+
+local incrementReducer = Rodux.createReducer(0, {
+	IncrementAction = function(state, action)
+		local newState = state or 0
+
+		newState += 1
+
+		return newState
+	end,
+})
+
+local timeReducer = Rodux.createReducer(0, {
+	TimeAction = function(state, action)
+		local newState = state or 0
+
+		newState += 1
+
+		return newState
+	end,
+})
+
+local reducer = Rodux.combineReducers({
+	value = incrementReducer,
+	gameTime = timeReducer,
+})
+
+local store = Rodux.Store.new(reducer)
+
+-- ROACT COMPONENTS
+
+-- Increment Button
+local function IncrementComponent(props)
+	-- Values from Rodux can be accessed just like regular props
+	local value = props.value
+	local onClick = props.onClick
+
+	return Roact.createElement("ScreenGui", nil, {
+		Label = Roact.createElement("TextButton", {
+			-- ...and used in your components!
+			Text = "Current value: " .. value,
+			Size = UDim2.new(0.25, 0, 0.25, 0),
+			Position = UDim2.new(0, 0, 0.25, 0),
+
+			[Roact.Event.Activated] = onClick,
+		}),
+	})
+end
+
+-- `connect` accepts two optional functions:
+-- `mapStateToProps` accepts your store's state and returns props
+-- `mapDispatchToProps` accepts a dispatch function and returns props
+
+-- Both functions should return a table containing props that will be passed to
+-- your component!
+
+-- `connect` returns a function, so we call that function, passing in our
+-- component, getting back a new component!
+IncrementComponent = RoactRodux.connect(function(state, props)
+	-- mapStateToProps is run every time the store's state updates.
+	-- It's also run whenever the component receives new props.
+	return {
+		value = state.value,
+	}
+end, function(dispatch)
+	-- mapDispatchToProps only runs once, so create functions here!
+	return {
+		onClick = function()
+			dispatch(IncrementAction())
+		end,
+	}
+end)(IncrementComponent)
+
+local Clock = Roact.Component:extend("Clock")
+
+function Clock:render()
+	return Roact.createElement("TextLabel", {
+		Text = "Time: " .. self.props.gameTime, -- The component just uses props
+		Size = UDim2.new(0, 200, 0, 50),
+	})
+end
+
+-- Create the connected component
+local ClockContainer = RoactRodux.connect(function(state, props)
+	-- mapStateToProps Clock
+	return {
+		gameTime = state.gameTime,
+	}
+end)(Clock)
+
+-- Rodux store available for any components
+local app = Roact.createElement(RoactRodux.StoreProvider, {
+	store = store,
+}, {
+	-- Miss used fragment
+	-- MyFragment = Roact.createFragment({
+	-- 	IncrementButton = Roact.createElement(IncrementComponent),
+	-- 	Clock = Roact.createElement(ClockContainer),
+	-- }),
+
+	-- MainGUI
+	Roact.createElement("ScreenGui", {}, {
+		IncrementButton = Roact.createElement(IncrementComponent),
+		Clock = Roact.createElement(ClockContainer),
+	}),
+})
+
+Roact.mount(app, PlayerGui)
+
+-- LIFETIME : store / GlobalState
+
+while true do
+	task.wait(1)
+	-- print(store.getState(store))
+	store:dispatch(TimeAction())
+end
---
+++ src/client/RoduxTest.client.lua
@@ -0,0 +1,140 @@
+-- Rodux Example : https://roblox.github.io/rodux/example/
+-- Can also be used Server Side
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local Rodux = require(ReplicatedStorage.Shared.Rodux)
+
+-- Action creator for the ReceivedNewPhoneNumber action
+local function ReceivedNewPhoneNumber(phoneNumber)
+	return {
+		type = "ReceivedNewPhoneNumber",
+		phoneNumber = phoneNumber,
+	}
+end
+
+-- Action creator for the MadeNewFriends action
+local function MadeNewFriends(listOfNewFriends)
+	return {
+		type = "MadeNewFriends",
+		newFriends = listOfNewFriends,
+	}
+end
+
+-- Reducer for the current user's phone number
+local phoneNumberReducer = Rodux.createReducer("", {
+	ReceivedNewPhoneNumber = function(state, action)
+		return action.phoneNumber
+	end,
+})
+
+-- Reducer for the current user's list of friends
+local friendsReducer = Rodux.createReducer({}, {
+	MadeNewFriends = function(state, action)
+		local newState = {}
+
+		-- Since state is read-only, we copy it into newState
+		for index, friend in ipairs(state) do
+			newState[index] = friend
+		end
+
+		for _, friend in ipairs(action.newFriends) do
+			table.insert(newState, friend)
+		end
+
+		return newState
+	end,
+})
+
+local reducer = Rodux.combineReducers({
+	myPhoneNumber = phoneNumberReducer,
+	myFriends = friendsReducer,
+})
+
+local store = Rodux.Store.new(reducer, nil, {
+	-- Rodux.loggerMiddleware,
+})
+
+store:dispatch(ReceivedNewPhoneNumber("12345678"))
+store:dispatch(MadeNewFriends({
+	"Cassandra",
+	"Joe",
+}))
+
+--[[
+    Expected output to the developer console:
+
+    Action dispatched: {
+        phoneNumber = "12345678" (string)
+        type = "ReceivedNewPhoneNumber" (string)
+    }
+    State changed to: {
+        myPhoneNumber = "12345678" (string)
+        myFriends = {
+        }
+    }
+    Action dispatched: {
+        newFriends = {
+            1 = "Cassandra" (string)
+            2 = "Joe" (string)
+        }
+        type = "MadeNewFriends" (string)
+    }
+    State changed to: {
+        myPhoneNumber = "12345678" (string)
+        myFriends = {
+            1 = "Cassandra" (string)
+            2 = "Joe" (string)
+        }
+    }
+]]
+
+-- EQUIVALENT :
+
+--[[
+local phoneNumberReducer = function(state, action)
+    if action.type == "ReceivedNewPhoneNumber" then
+        return action.phoneNumber
+    end
+
+    return state
+end
+]]
+
+--[[
+local friendsReducer = function(state, action)
+        -- The state might be nil the first time this reducer is executed.
+        -- In that case, we need to initialize our state to be the empty table.
+    state = state or {}
+
+    if action.type == "MadeNewFriends" then
+        local newState = {}
+
+        -- Since state is read-only, we copy it into newState
+        for index, friend in ipairs(state) do
+            newState[index] = friend
+        end
+
+        for _, friend in ipairs(action.newFriends)
+            table.insert(newState, friend)
+        end
+
+        return newState
+    end
+
+    return state
+end
+]]
+
+--[[
+    note that the reducer for our entire application is defined by a table of
+    sub-reducers where each sub-reducer is responsible for one portion of the
+    overall state.
+
+local reducer = function(state, action)
+    return {
+        myPhoneNumber = phoneNumberReducer(state.myPhoneNumber, action),
+        myFriends = friendsReducer(state.myFriends, action),
+    }
+end
+]]
